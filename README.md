1d5318561ea21c0df57f61149cd5ee60d3801946
chase colvin <colvin.chase@yahoo.com>
2022-03-23 22:22:22 -0500

    add code

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # caesars-cipher
+Encryption/Decryption Function using Caesars Cipher
+1000 chars available in this program for the message you want to encrypt, key must be an int not a float or char.
---
+++ encryption.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+int main()
+{
+    char plaintext[1000], ch;
+        int i, key;
+        printf("Type a message you want encrypted using Caesars Cyper: ");
+        gets(plaintext);
+        //enter an INT for key, not a float or a char 
+        //for ex.. a key value of 1; plaintext "Hello World" = "Ifmmp Xpsme", and so forth.. 
+        printf("Enter Key Value: ");
+        scanf("%d", & key);
+            for(i = 0; plaintext[i] != '\0'; ++i)
+        {
+            ch = plaintext[i];
+            if (ch >= 'A' && ch <= 'Z')
+            {
+                ch = ch + key;
+            
+            if (ch > 'Z')
+            {
+                ch = ch - 'Z' + 'A' - 1;
+            }
+            plaintext[i] = ch;
+            }
+
+            else if (ch >= 'a' && ch <= 'z')
+            {
+                ch = ch + key;
+            }
+            if (ch > 'z')
+            {
+                ch = ch - 'z' + 'z' - 1;
+            }
+            plaintext[i] = ch;
+            }
+            printf("Encrypted message: %s", plaintext);
+            return 0;
+}
