06673f0aad2d8120819bfd6c8a5b574320f8dc7a
chase colvin <colvin.chase@yahoo.com>
2022-03-24 21:49:52 -0500

    reformat code

--- decryption.c
+++ decryption.c
@@ -1,38 +1,38 @@
-#include<stdio.h>
- 
+#include <stdio.h>
+
 int main()
 {
     char plaintext[1000], ch;
-        int i, key;
-        printf("Type a message you want decrypted using Caesars Cypher: ");
-        gets(plaintext);
-        printf("Enter Key Value used in prior text encryption: "); 
-        scanf("%d", & key);
-         for(i = 0; plaintext[i] != '\0'; i++)
+    int i, key;
+    printf("Type a message you want decrypted using Caesars Cypher: ");
+    gets(plaintext);
+    printf("Enter Key Value used in prior text encryption: ");
+    scanf("%d", &key);
+    for (i = 0; plaintext[i] != '\0'; i++)
+    {
+        ch = plaintext[i];
+        if (ch >= 'A' && ch <= 'Z')
         {
-            ch = plaintext[i];
-            if (ch >= 'A' && ch <= 'Z')
-            {
-                ch = ch - key;
-            
+            ch = ch - key;
+
             if (ch < 'Z')
             {
                 ch = ch + 'Z' - 'A' + 1;
             }
             plaintext[i] = ch;
-            }
+        }
 
-            else if (ch >= 'a' && ch <= 'z')
-            {
-                ch = ch - key;
-            }
-            if (ch < 'a')
-            {
-                ch = ch + 'z' - 'a' + 1;
-            }
-            plaintext[i] = ch;
-            }
-            printf("Decrypted message: %s", plaintext);
-            return 0;
-}
+        else if (ch >= 'a' && ch <= 'z')
+        {
+            ch = ch - key;
+        }
+        if (ch < 'a')
+        {
+            ch = ch + 'z' - 'a' + 1;
+        }
+        plaintext[i] = ch;
+    }
     
+    printf("Decrypted message: %s", plaintext);
+    return 0;
+}
